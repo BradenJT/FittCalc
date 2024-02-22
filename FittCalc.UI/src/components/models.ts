7154b4907eb4ae375b04d31976300034351a93ff
BradenJT <bradenjparrish@gmail.com>
2024-02-21 21:06:16 -0600

    Created basic layout and calc functionality

--- FittCalc.UI/src/components/models.ts
+++ FittCalc.UI/src/components/models.ts
@@ -6,3 +6,13 @@ export interface Todo {
 export interface Meta {
   totalCount: number;
 }
+
+export interface GenderOption {
+  label: string;
+  value: 'male' | 'female';
+}
+
+export interface ActivityOption {
+  label: string;
+  value: 'sedentary' | 'lightly' | 'moderately' | 'very' | 'extra';
+}
---
+++ FittCalc.UI/src/modules/pages/Home/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <calorie-calc></calorie-calc>
+  </q-page>
+</template>
+
+<script lang="ts">
+import CalorieCalc from 'src/modules/pages/Home/components/CalorieCalc.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { CalorieCalc },
+});
+</script>
